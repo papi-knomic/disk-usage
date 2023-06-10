62bb1d9b0374fd106d449072b11ad6dc4540ea12
Samson Moses <knomic10@gmail.com>
2023-06-10 02:43:15 +0100

    first commit

---
+++ disk-usage.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Disk Usage
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Samson Moses
+Author URI: https://github.com/papi-knomic
+License: A "Slug" license name e.g. GPL2
+*/
